a5f8948e4424dcd2297d32180fe9b9430b2feb9e
fuyuukurogi <lostheaven.jmt@gmail.com>
2018-06-24 16:56:44 -0300

    teste2

--- application/views/common/navbar.php
+++ application/views/common/navbar.php
@@ -6,7 +6,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar mdb-color lighten-2 mb-5">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <strong>Módulo Área</strong>
+                <strong><italic>Módulo Área</italic></strong>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7"
                 aria-expanded="false" aria-label="Toggle navigation">
